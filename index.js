73388b0ee23dc6608b51f3a830c746aef7865c7a
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-02-28 09:58:41 -0500

    Save before branching

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require("express")
 const methodOverride = require("method-override")
 const parser = require("body-parser")
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+const hbs = exphbs.create({extname: '.hbs'});
 const app = express()
 
-app.set("view engine", "hbs");
+app.engine('handlebars', hbs.engine);
+
+app.set('views', __dirname + '/views/');
+app.use(express.static("public"))
+
+app.set('view engine', hbs.extname);
+
+
 app.set("port", process.env.PORT || 1874)
 
-app.use(express.static("public"))
 app.use(methodOverride("_method"))
 app.use(express.urlencoded({extended: true}))
 app.use(parser.urlencoded({extended: true}))
 app.use(require("./routes/index"))
+
 
 app.listen(app.get("port"), () => {
     console.log(`✅ PORT: ${app.get("port")} 🌟`)
